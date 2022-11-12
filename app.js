e5427b52609f7d958dbf4f4c1e3439c59e8e4f56
Tushar Khatri <khatritushar320@gmail.com>
2022-11-12 11:37:36 +0530

    Add get and post requests for login and register in app.js

--- app.js
+++ app.js
@@ -9,7 +9,25 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
-    res.render("home");  
+    res.render("index");  
+});
+
+app.get("/signin", (req, res) => {
+    res.render("signin");
+});
+
+app.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+app.get("/home", (req, res) => {
+    res.render("index");
+});
+
+app.post("/home", (req, res) => {
+    let name = req.body.name;
+    console.log(req.body);
+    // res.render("home")
 });
 
 app.listen(PORT, "0.0.0.0", () => {
